57af550366797af797cd3e8fed410316ed532bbd
ericlumsden <elumsden@uw.edu>
2022-06-12 22:18:27 -0700

    subFuncs

--- functions/apFind.py
+++ functions/apFind.py
@@ -1,19 +1,36 @@
 import numpy as np
+import apISI
+from subFuncs import *
 '''
-The function 'apFind' is used to find all action potentials in a given trace
+The function 'apFind' is used to find all action potentials in a given trace and run the subsequent functions for analyzing each action potential
 Action potentials are identified based on a threshold crossing which has a default value of -20.0 mV but can be changed
-This function returns an array of indices for action potential peaks
 '''
 def apFind(trace, threshold=-20.0):
-    apPeaks = [] # Initiate an empty array for the action potential peaks
+    # Initiate empty arrays for all of the measurements
+    apPeaks = []
+    apRise = []
+    apDecay = []
+    apHalfWidth = []
+    apAHPmin = []
+    apAHPlen = []
+
     start = 0 # Start of threshold crossing for each AP
-    for idx, val in enumerate(trace):
+    for idx, val in enumerate(trace, buffer):
         if (val < threshold) and (start == 0):
             continue
         elif (val > threshold) and (start == 0):
             start = idx
         elif (val < threshold) and (start != 0):
-            apPeaks.append(np.argmax[start:idx])
+            peakIDX = np.argmax[start:idx] + start
+            subTrace = trace[int(peakIDX-buffer):int(peakIDX+buffer)]
+            apPeaks.append(peakIDX)
+            apRise.append(rise(subTrace, peakIDX))
+            apDecay.append(decay(subTrace, peakIDX))
+            apHalfWidth.append(halfWidth(subTrace, peakIDX))
+            apAHPmin.append(ahpMin(subTrace, peakIDX))
+            apAHPlen.append(ahpLen(subTrace, peakIDX))
             start = 0 # Need to reset for next AP
+
+        apInterSpikeIntervals = apISI(apPeaks)
         
-    return apPeaks
+    return apPeaks, apInterSpikeIntervals, apRise, apDecay, apHalfWidth, apAHPmin, apAHPlen
---
+++ functions/subFuncs.py
@@ -0,0 +1,19 @@
+'''
+Rather than constantly iterating through each trace, I will find AP parameters when the peaks are discovered
+Here I will define the subfunctions called in apFind at the time of AP peak discovery
+'''
+
+def rise():
+    return r
+
+def decay():
+    return d
+
+def halfWidth():
+    return hw
+
+def ahpMin():
+    return ahpM
+
+def ahpLen():
+    return ahpL
