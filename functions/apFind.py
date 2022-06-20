df198cb43e189c28e734afd88a0c665a6969f433
ericwlumsden <airique21@gmail.com>
2022-06-20 11:39:46 -0700

    rise and decay

--- functions/apFind.py
+++ functions/apFind.py
@@ -12,7 +12,7 @@ For other AP properties, a 'sub_trace' is utilized, which takes the index of the
     ...that total stretch is made into an array and then passed into subsequent functions
 
 '''
-def apFind(trace, threshold=-20.0, buffer=int(0.01 * 10000)):
+def apFind(trace, threshold=-20.0, buff=0.01, freq=10000)):
     # Initiate empty arrays for all of the measurements
     apPeaks = []
     apRise = []
@@ -21,6 +21,9 @@ def apFind(trace, threshold=-20.0, buffer=int(0.01 * 10000)):
     apAHPmin = []
     apAHPlen = []
     apInterSpikeIntervals = []
+
+    # Set buffer
+    buffer = int(buff * freq)
 
     start = 0 # Start of threshold crossing for each AP
     for idx, val in enumerate(trace):
@@ -31,18 +34,21 @@ def apFind(trace, threshold=-20.0, buffer=int(0.01 * 10000)):
             # Find the peak of the AP based on the start/end of threshold crossing...
             peak_idx = np.argmax[start:idx] + start
             apPeaks.append(peak_idx)
-            # Find inter-spike interval
+
+            # Find inter-spike interval by taking difference in the indices of this AP and the previous one, then dividing by sampling frequency
             if (ap_count > 1):
-                apInterSpikeIntervals.append((peak_idx - apPeaks[ap_count]) * 10000)
-            # ...then get the subtrace array based on that peak and the buffer
+                apInterSpikeIntervals.append((peak_idx - apPeaks[ap_count]) / freq)
+
+            # Get the subtrace array based on the peak and the buffer
             sub_trace = trace[int(peak_idx - buffer):int(peak_idx + buffer)]
 
             # Use the max of the derivative to find the AP takeoff point (this will be used in sub functions)
-            dVdt = np.diff(sub_trace) / np.diff(len(sub_trace)/10000)
-            ap_takeoff = np.argmax(dVdt) + start
+            ap_takeoff = apTakeoff(sub_trace, freq) + start # have to include `+ start` as the function just finds the max of the derivative of the array passed
+            # Then use the takeoff point to find where the AP returns to baseline
+            ap_return = apReturn(sub_trace, (ap_takeoff - start), freq) + start
 
-            apRise.append(rise(sub_trace, peak_idx))
-            apDecay.append(decay(sub_trace, peak_idx))
+            apRise.append((peak_idx - ap_takeoff) / freq)
+            apDecay.append((ap_return - peak_idx) / freq)
             apHalfWidth.append(halfWidth(sub_trace, peak_idx))
             apAHPmin.append(ahpMin(sub_trace, peak_idx))
             apAHPlen.append(ahpLen(sub_trace, peak_idx))
--- functions/subFuncs.py
+++ functions/subFuncs.py
@@ -1,13 +1,26 @@
+import numpy as np
 '''
 Rather than constantly iterating through each trace, I will find AP parameters when the peaks are discovered
 Here I will define the subfunctions called in apFind at the time of AP peak discovery
 '''
 
-def rise():
-    return r
+# To find the AP takeoff point, return the maximum value of the first derivative of the AP
+def apTakeoff(ap_array, freq):
+    dVdt = np.diff(ap_array) / np.diff(len(ap_array)/freq)
+    return np.argmax(dVdt) 
 
-def decay():
-    return d
+# apReturn finds the index at which the AP returns to the same value as the takeoff point (for finding halfwidth and decay)
+def apReturn(ap_array, takeoff, freq):
+    target = ap_array[int(takeoff)]
+    
+    for idx, x in enumerate(ap_array[int(takeoff):]):
+        if x > target:
+            continue
+        else:
+            bl_return = idx
+            break
+    
+    return bl_return
 
 def halfWidth():
     return hw
