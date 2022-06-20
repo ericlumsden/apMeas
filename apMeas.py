fb3f7881da55bf65122173030f5a7a36a526fc74
ericwlumsden <airique21@gmail.com>
2022-06-20 13:46:48 -0700

    created main apMeas file

---
+++ apMeas.py
@@ -0,0 +1,9 @@
+from functions.apFind import *
+
+'''
+In the main apMeas script I will load a csv file with all of the info for the traces to be analyzed
+
+Qs that remain:
+- Will I save each file as its own dataclass?
+- How do I want to deal with spont vs induced APs?
+'''
